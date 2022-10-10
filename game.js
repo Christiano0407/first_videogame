e953afb8325f683e03603bb8ed6310291c2a5500
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-10 16:38:27 +0000

    add new canvas

--- game.js
+++ game.js
@@ -1,1 +1,30 @@
 //*TODO === === === Games === === === === */
+const canvas = document.querySelector(`#game`);
+const gameCtx = canvas.getContext("2d");
+let canvasSize;
+let elementSize;
+//** === Code */
+
+/* const startGame = () => {
+  gameCtx.fillRect(0, 0, 100, 100);
+  gameCtx.clearRect(50, 50, 50, 50);
+  gameCtx.font = `25px Verdana`;
+  gameCtx.fillStyle = "purple";
+  gameCtx.fillText("Platzi", 50, 50);
+};
+ */
+
+const startGame = () => {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+
+  canvas.setAttribute("Width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
+
+  elementSize = canvasSize / 10;
+  console.log(canvasSize, elementSize);
+};
+window.addEventListener("load", startGame);
