139a80e6143f75bc9c9ea2ff221c9d26e6e43181
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-17 20:19:41 +0000

    game

--- game/gamePlay.js
+++ game/gamePlay.js
@@ -1,1 +1,8 @@
 //** === Video Game Javascript ===  */
+const canvas = document.querySelector(`#game`);
+const game = canvas.getContext(`2d`);
+
+const startGame = () => {};
+
+window.addEventListener("load", startGame);
+//** === === === Load Window === === ===  */
