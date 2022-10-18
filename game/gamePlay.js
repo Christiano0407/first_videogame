465c0243adec08428d4748ce882d942c12bb242a
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-18 17:16:56 +0000

    canvas part fill

--- game/gamePlay.js
+++ game/gamePlay.js
@@ -1,8 +1,25 @@
 //** === Video Game Javascript ===  */
+//**TODO === === === cONST & Variable === === ===  */
 const canvas = document.querySelector(`#game`);
 const gameCTX = canvas.getContext(`2d`);
+let canvasSize;
+let elementSize;
 
-const startGame = () => {};
+//*! === === === === CODE === === === === */
+const startGame = () => {
+  window.innerHeight > window.innerWidth
+    ? (canvasSize = window.innerWidth * 0.8)
+    : (canvasSize = window.innerHeight * 0.8);
 
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
+
+  elementSize = canvasSize / 10;
+  console.log(elementSize);
+  gameCTX.font = elementSize + "px Arial";
+  gameCTX.textAlign = "end";
+  gameCTX.fillText(emojis["X"], elementSize, elementSize);
+};
+
+//*? === === === Load Window === === ===  */
 window.addEventListener("load", startGame);
-//** === === === Load Window === === ===  */
