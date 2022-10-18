51d0746cf282c68183dfed664e2fedd4e4419487
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-18 20:27:29 +0000

    canvas map challenge 02

--- game/gamePlay.js
+++ game/gamePlay.js
@@ -4,9 +4,13 @@ const canvas = document.querySelector(`#game`);
 const gameCTX = canvas.getContext(`2d`);
 let canvasSize;
 let elementSize;
+let map;
+let rowMap;
+let colMap;
 
 //*! === === === === CODE === === === === */
-const startGame = () => {
+
+const setCanvasSize = () => {
   window.innerHeight > window.innerWidth
     ? (canvasSize = window.innerWidth * 0.8)
     : (canvasSize = window.innerHeight * 0.8);
@@ -16,12 +20,28 @@ const startGame = () => {
 
   elementSize = canvasSize / 10;
 
-  gameCTX.font = elementSize + "px Arial";
-  gameCTX.textAlign = "end";
-  /*  for (let i = 1; i <= 10; i++) {
-} */
-  gameCTX.fillText(emojis["X"], elementSize, elementSize);
+  startGame();
 };
 
+const startGame = () => {
+  gameCTX.font = elementSize + "px Arial";
+  gameCTX.textAlign = "end";
+
+  map = maps[0];
+  rowMap = map.trim().split("\n");
+  colMap = rowMap.map((row) => row.trim().split(""));
+  console.log({ map, rowMap, colMap });
+
+  for (let row = 1; row <= 10; row++) {
+    for (let col = 1; col <= 10; col++) {
+      gameCTX.fillText(
+        emojis[colMap[row - 1][col - 1]],
+        elementSize * col,
+        elementSize * row
+      );
+    }
+  }
+};
 //*? === === === Load Window === === ===  */
-window.addEventListener("load", startGame);
+window.addEventListener("resize", setCanvasSize);
+window.addEventListener("load", setCanvasSize);
